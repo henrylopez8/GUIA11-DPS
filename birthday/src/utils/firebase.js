5ef79a928ac1523834c61e06ce51a7567e773ebc
henrylopez8 <henrylopez1998.hfl@gmail.com>
2021-11-05 20:19:30 -0600

    version 1

---
+++ birthday/src/utils/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import firebase from "firebase";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyDbo7wOJ8XKob1bTfvjaRSjEWd-YKk1Ja4",
+    authDomain: "birthday-770db.firebaseapp.com",
+    projectId: "birthday-770db",
+    storageBucket: "birthday-770db.appspot.com",
+    messagingSenderId: "690604454134",
+    appId: "1:690604454134:web:a9bcbdaf754b6a89c77cd7"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+export default firebase;
